d32603115b0ed8ec3e394300ccf25e0d795813df
Vintache Jean <jean.vintache@etu.utc.fr>
2016-05-27 14:13:08 +0200

    add request normalisation before syntaxic analysis

--- Analyseur_syntaxique/src/Main.java
+++ Analyseur_syntaxique/src/Main.java
@@ -10,21 +10,27 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("Texte : ");
-		
-		
+		// input natural language request
 		String s = scanner.nextLine();
-		
+		// get normalized request
+		s = Normalisator.normalize(s);
+		// print normalized request
 		System.out.println(s);
+
 		while (!s.equals("*")) {
 			try {
-				lo17SqlGrammarLexer lexer = new lo17SqlGrammarLexer(new ANTLRReaderStream(
-						new StringReader(s)));
+				lo17SqlGrammarLexer lexer = new lo17SqlGrammarLexer(new ANTLRReaderStream(new StringReader(s)));
 				CommonTokenStream tokens = new CommonTokenStream(lexer);
 				lo17SqlGrammarParser parser = new lo17SqlGrammarParser(tokens);
+
 				String arbre = parser.listerequetes();
+
 				System.out.println(arbre);
-			} catch (Exception e) {
 			}
+			catch (Exception e) {
+
+			}
+
 			System.out.print("Texte : ");
 			s = scanner.nextLine();
 		}
---
+++ Analyseur_syntaxique/src/Normalisator.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+
+public class Normalisator {
+
+	public static String normalize(String request) {
+		Lexique lexique = new Lexique("res/lemmes");
+		
+		// extraction des mots de la requête
+		StringTokenizer st = new StringTokenizer(request);
+		ArrayList<String> motsRequete = new ArrayList<String>();
+		while (st.hasMoreTokens()) {
+	         motsRequete.add(st.nextToken().toLowerCase());
+	    }
+
+		String result = "";
+
+		for (String string : motsRequete) {
+			ArrayList<String> lemmesCandidats;
+
+			// 2)b
+			if(lexique.getDictionnaire().containsKey(string)){
+				result += lexique.getDictionnaire().get(string) + " ";
+			}
+			// 2)c
+			else if((lemmesCandidats = lexique.proximitePrefixe(string)).size()>0){
+				// take first candidate ?
+				result += lemmesCandidats.get(0) + " ";
+			}
+			// 2)c
+			else if((lemmesCandidats = lexique.proximiteLevenshtein(string)).size()>0){
+				// take first candidate ?
+				result += lemmesCandidats.get(0) + " ";
+			}
+		}
+
+		return result.trim();
+	}
+}
